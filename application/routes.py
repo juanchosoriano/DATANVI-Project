7ec009bfdcec7115e47eed62a7f3810fde2c367a
angelinegubat <angeline_maris_gubat@dlsu.edu.ph>
2021-09-22 00:26:18 +0800

    graph edits
    Co-Authored-By: Jj Reyes <47908236+jjbreyes@users.noreply.github.com>
    Co-Authored-By: juanchosoriano <61454422+juanchosoriano@users.noreply.github.com>

--- application/routes.py
+++ application/routes.py
@@ -46,156 +46,136 @@ GameAnxiety['SWL_T'] = GameAnxiety[col_list].sum(axis=1)
 GAD_TOTAL = 21
 SWL_TOTAL = 30
 SPIN_TOTAL = 68
+TESTS_TOTAL = GAD_TOTAL + SWL_TOTAL + SPIN_TOTAL
 
 Gamer_Age = GameAnxiety[['Age', 'GAD_T', 'SWL_T', 'SPIN_T']]
+Game_Age_Copy = GameAnxiety[['Age', 'GAD_T', 'SWL_T', 'SPIN_T']]
+
 n_age_group = 5
 age_max = Gamer_Age['Age'].max()
 age_min = Gamer_Age['Age'].min()
-age_n_range = (age_max - age_min) / n_age_group
-Gamer_Age['Age'] = pd.cut(Gamer_Age['Age'], n_age_group)
 
-Gamer_Age_Total = Gamer_Age.groupby(['Age']).sum()
-Gamer_Age_Total['Gamers In Category'] = Gamer_Age.groupby(['Age']).size()
-Gamer_Age_Total['GAD_Ave'] = Gamer_Age_Total['GAD_T'] / Gamer_Age_Total['Gamers In Category'] #Higher = more anxiety
-Gamer_Age_Total['SWL_Ave'] = Gamer_Age_Total['SWL_T'] / Gamer_Age_Total['Gamers In Category'] #Lower = more anxiety
-Gamer_Age_Total['SPIN_Ave'] = Gamer_Age_Total['SPIN_T'] / Gamer_Age_Total['Gamers In Category'] #Higher = more anxiety
-Gamer_Age_Total['Average Result'] = ((Gamer_Age_Total['GAD_Ave']/GAD_TOTAL) + ((SWL_TOTAL - Gamer_Age_Total['SWL_Ave']) / SWL_TOTAL) + (Gamer_Age_Total['SPIN_Ave'] / SPIN_TOTAL)) / 3 * 100
+Gamer_Age['Age'] = pd.cut(Gamer_Age['Age'], n_age_group)
+age_n_range = (age_max - age_min) / n_age_group
+GA_grouped = Gamer_Age.groupby('Age').sum()
+intervals = GA_grouped.index
+
+i = age_min
+age_ranges = []
+while(i < age_max):
+    ages = str(math.ceil(i)) + " to " + str(math.floor(i + age_n_range))
+    age_ranges.append(ages)
+    i += age_n_range
+
+for i in range(len(age_ranges)):
+    Gamer_Age['Age'] = np.where(Gamer_Age['Age'] == intervals[i], age_ranges[i], Gamer_Age['Age'])
+
+GamerSplitG = Gamer_Age[['Age', 'GAD_T']]
+GamerSplitG["Questionnaire Type"] = "GAD"
+GamerSplitW = Gamer_Age[['Age', 'SWL_T']]
+GamerSplitW["Questionnaire Type"] = "SWL"
+GamerSplitP = Gamer_Age[['Age', 'SPIN_T']]
+GamerSplitP["Questionnaire Type"] = "SPIN"
+
+GamerFlatten = pd.DataFrame({'Age': GamerSplitG['Age'], 'Type': GamerSplitG['Questionnaire Type'], 'Score': GamerSplitG['GAD_T']})
+GamerFlattenP = pd.DataFrame({'Age': GamerSplitP['Age'], 'Type': GamerSplitP['Questionnaire Type'], 'Score': GamerSplitP['SPIN_T']})
+GamerFlattenW = pd.DataFrame({'Age': GamerSplitW['Age'], 'Type': GamerSplitW['Questionnaire Type'], 'Score': GamerSplitW['SWL_T']})
+GamerFlatten = GamerFlatten.append(GamerFlattenP, ignore_index = True)
+GamerFlatten = GamerFlatten.append(GamerFlattenW, ignore_index = True)
+GamerFlatten = GamerFlatten.sort_values('Age')
+
+playstyle = GameAnxiety[['Playstyle', 'GAD_T', 'SWL_T', 'SPIN_T']]
+Gamer_Reason = GameAnxiety[['whyplay', 'GAD_T', 'SWL_T', 'SPIN_T']]
 
 user_age = 19
 @app.route('/')
 def index():
     return render_template('index.html', title = "Home Page")
 
-@app.route('/ageGroup', methods=['POST'])
-def textInput():
-    text = int(request.form['text'])
-    
-    user_age = text
-    age_groups = list(Gamer_Age_Total.index)
-    for group in age_groups:
-        if user_age in group:
-            user_group = group
-
-    user_row = Gamer_Age_Total.loc[user_group]
-    ave_res = user_row['Average Result']
-    data = {'Categories':['Game Anxiety', 'No Game Anxiety'], 'Value': [ ave_res, 100-ave_res, ]}
-    df = pd.DataFrame(data)
-    fig = px.pie(df, values='Value', names='Categories',  color = 'Categories', color_discrete_map={'No Game Anxiety': 'gainsboro', 'Game Anxiety': 'mediumorchid'})
-    fig.update_traces(hole=.6, hoverinfo="label+percent+name",  sort = False)
-
-    graph1JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template('Age Group.html', graph1JSON=graph1JSON)
-
 @app.route('/ageGroup')
 def ageGroup():
-    age_groups = list(Gamer_Age_Total.index)
-    for group in age_groups:
-        if user_age in group:
-            user_group = group
-
-    user_row = Gamer_Age_Total.loc[user_group]
-    ave_res = user_row['Average Result']
-    data = {'Categories':['Game Anxiety', 'No Game Anxiety'], 'Value': [ ave_res, 100-ave_res, ]}
-    df = pd.DataFrame(data)
-    fig = px.pie(df, values='Value', names='Categories',  color = 'Categories', color_discrete_map={'No Game Anxiety': 'gainsboro', 'Game Anxiety': 'mediumorchid'})
-    fig.update_traces(hole=.6, hoverinfo="label+percent+name",  sort = False)
-
-    graph1JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    Age2_fig = px.strip(GamerFlatten, x='Score', y="Type", color='Age', color_discrete_map = {"18 to 25": "#6F5F90", "26 to 33": "#FF7B89", "34 to 40": "#8A5082", "41 to 48": "#758EB7", "49 to 56": "#A5CAD2" })
+    graph1JSON = json.dumps(Age2_fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('Age Group.html', graph1JSON=graph1JSON)
 
 @app.route('/Gender')
 def Gender():
     GamerGender = GameAnxiety[['Gender', 'GAD_T', 'SWL_T', 'SPIN_T']]
-    GamerGender_total = GamerGender.groupby(['Gender']).sum()
-    GamerGender_total['Gamers in Category'] = GamerGender.groupby(['Gender']).size()
-    GamerGender_total['GAD_Ave'] = GamerGender_total['GAD_T'] / GamerGender_total['Gamers in Category']
-    GamerGender_total['SWL_Ave'] = GamerGender_total['SWL_T'] / GamerGender_total['Gamers in Category']
-    GamerGender_total['SPIN_Ave'] = GamerGender_total['SPIN_T'] / GamerGender_total['Gamers in Category']
-    GamerGender_total['Average Result'] = ((GamerGender_total['GAD_Ave']/GAD_TOTAL) + ((SWL_TOTAL - GamerGender_total['SWL_Ave']) / SWL_TOTAL) + (GamerGender_total['SPIN_Ave'] / SPIN_TOTAL)) / 3 * 100
-    
-    fig = px.pie(GamerGender_total, values = 'Average Result', names = GamerGender_total.index, color = GamerGender_total.index, color_discrete_map={'Other' : '#B399D4', 'Male': '#96B9D0', 'Female': '#EBA7AC'})
-    graph2JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+
+    GamerSplitG2 = GamerGender[['Gender', 'GAD_T']]
+    GamerSplitG2["Questionnaire Type"] = "GAD"
+    GamerSplitW2 = GamerGender[['Gender', 'SWL_T']]
+    GamerSplitW2["Questionnaire Type"] = "SWL"
+    GamerSplitP2 = GamerGender[['Gender', 'SPIN_T']]
+    GamerSplitP2["Questionnaire Type"] = "SPIN"
+
+    GamerFlatten2 = pd.DataFrame({'Gender': GamerSplitG2['Gender'], 'Type': GamerSplitG2['Questionnaire Type'], 'Score': GamerSplitG2['GAD_T']})
+    GamerFlattenP2 = pd.DataFrame({'Gender': GamerSplitP2['Gender'], 'Type': GamerSplitP2['Questionnaire Type'], 'Score': GamerSplitP2['SPIN_T']})
+    GamerFlattenW2 = pd.DataFrame({'Gender': GamerSplitW2['Gender'], 'Type': GamerSplitW2['Questionnaire Type'], 'Score': GamerSplitW2['SWL_T']})
+    GamerFlatten2 = GamerFlatten2.append(GamerFlattenP2, ignore_index = True)
+    GamerFlatten2 = GamerFlatten2.append(GamerFlattenW2, ignore_index = True)
+    GamerFlatten2 = GamerFlatten2.sort_values('Gender')
+
+    Gender_fig = px.strip(GamerFlatten2, x='Score', y="Type", color='Gender', color_discrete_map = {"Male": "#96B9D0", "Female": "#EBA7AC", "Other": "#B399D4"})
+
+    graph2JSON = json.dumps(Gender_fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('Gender.html', graph2JSON=graph2JSON)
 
 @app.route('/Playstyle')
 def Playstyle():
-    playstyle = GameAnxiety[['Playstyle', 'GAD_T', 'SWL_T', 'SPIN_T']]
-    playstyle_Total = playstyle.groupby(['Playstyle']).sum()
-    playstyle_Total['Gamers in Category'] = playstyle.groupby(['Playstyle']).size()
+    
 
-    playstyle_df = playstyle_Total.nlargest(4, columns=['Gamers in Category'])
+    playstyle.loc[(playstyle['Playstyle'] != "Singleplayer") & (playstyle['Playstyle'] != "Multiplayer - Online") &(playstyle['Playstyle'] != "Multiplayer - Offline") & (playstyle['Playstyle'] != "All of the above"), 'Playstyle'] = "Others"
+    GamerSplitG4 = playstyle[['Playstyle', 'GAD_T']]
+    GamerSplitG4["Questionnaire Type"] = "GAD"
+    GamerSplitW4 = playstyle[['Playstyle', 'SWL_T']]
+    GamerSplitW4["Questionnaire Type"] = "SWL"
+    GamerSplitP4 = playstyle[['Playstyle', 'SPIN_T']]
+    GamerSplitP4["Questionnaire Type"] = "SPIN"
 
-    playstyle_df.loc[len(playstyle_df)] = [playstyle_Total.drop(playstyle_df.index)['GAD_T'].sum(), playstyle_Total.drop(playstyle_df.index)['SWL_T'].sum(), playstyle_Total.drop(playstyle_df.index)['SPIN_T'].sum(), playstyle_Total.drop(playstyle_df.index)['Gamers in Category'].sum()]
-    playstyle_df = playstyle_df.rename(index={4: 'Others'})
+    GamerFlatten4 = pd.DataFrame({'Playstyle': GamerSplitG4['Playstyle'], 'Type': GamerSplitG4['Questionnaire Type'], 'Score': GamerSplitG4['GAD_T']})
+    GamerFlattenP4 = pd.DataFrame({'Playstyle': GamerSplitP4['Playstyle'], 'Type': GamerSplitP4['Questionnaire Type'], 'Score': GamerSplitP4['SPIN_T']})
+    GamerFlattenW4 = pd.DataFrame({'Playstyle': GamerSplitW4['Playstyle'], 'Type': GamerSplitW4['Questionnaire Type'], 'Score': GamerSplitW4['SWL_T']})
+    GamerFlatten4 = GamerFlatten4.append(GamerFlattenP4, ignore_index = True)
+    GamerFlatten4 = GamerFlatten4.append(GamerFlattenW4, ignore_index = True)
+    GamerFlatten4 = GamerFlatten4.sort_values('Playstyle', ascending=False)
 
-    playstyle_df['GAD_Ave'] = playstyle_df['GAD_T'] / playstyle_df['Gamers in Category']
-    playstyle_df['SWL_Ave'] = playstyle_df['SWL_T'] / playstyle_df['Gamers in Category']
-    playstyle_df['SPIN_Ave'] = playstyle_df['SPIN_T'] / playstyle_df['Gamers in Category']
-    playstyle_df['Average_Result'] = round(((playstyle_df['GAD_Ave']/GAD_TOTAL) + ((SWL_TOTAL - playstyle_df['SWL_Ave']) / SWL_TOTAL) + (playstyle_df['SPIN_Ave'] / SPIN_TOTAL)) / 3, 4)
-
-    playstyle_df.sort_values(by=['Gamers in Category'], ascending = False)
-
-    Play_Style = list(playstyle_df.index)
-    Average_resultPS =  playstyle_df['Average_Result']
-    style_of_play = plt.barh(Play_Style, Average_resultPS)
-    fig = px.bar(style_of_play, Average_resultPS, Play_Style, text=[f'{i:.2f}%' for i in Average_resultPS*100],
-            color = Play_Style, color_discrete_map={
-                "Multiplayer - Offline": "#6F5F90",
+    Playstyle_fig = px.strip(GamerFlatten4, x='Score', y="Type", color='Playstyle', color_discrete_map = {"Multiplayer - Offline": "#6F5F90",
                 "Multiplayer - Online": "#FF7B89",
                 "Singleplayer": "#8A5082",
                 "All of the above": "#758EB7",
-                "Others": "#A5CAD2"}, 
-                labels={
-                     "x": "Average Results (in percentage) for each playstyle",
-                     "y": "Playstyle"
-                 }, title="How does the playstyle of the gamer affect anxiety?", orientation='h', width=800, height = 350)
-    fig.update_layout(showlegend=False)
-    fig.layout.xaxis.tickformat = '0.00%'
-    fig.update_layout(xaxis_range=[0,1])
-    graph3JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+                "Others": "#A5CAD2"})
+    graph3JSON = json.dumps(Playstyle_fig, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template('Playstyle.html', graph3JSON=graph3JSON)
 
 @app.route('/Reason')
 def Reason():
-    Gamer_Reason = GameAnxiety[['whyplay', 'GAD_T', 'SWL_T', 'SPIN_T']]
-    Gamer_Reason_Total = Gamer_Reason.groupby(['whyplay']).sum()
-    Gamer_Reason_Total['Gamers In Category'] = Gamer_Reason.groupby(['whyplay']).size()
-    #Gamer_Reason_Total.loc[Gamer_Reason_Total['Gamers In Category'] < 2, 'whyplay'] = "Others"
-    Gamer_Reason_df = Gamer_Reason_Total.nlargest(5, columns=['Gamers In Category'])
+    
+    Gamer_Reason.loc[(Gamer_Reason['whyplay'] != "having fun") & (Gamer_Reason['whyplay'] != "improving") &(Gamer_Reason['whyplay'] != "winning") & (Gamer_Reason['whyplay'] != "relaxing") & (Gamer_Reason['whyplay'] != "All of the above"), 'whyplay'] = "Others"
+   
+    GamerSplitG3 = Gamer_Reason[['whyplay', 'GAD_T']]
+    GamerSplitG3["Questionnaire Type"] = "GAD"
+    GamerSplitW3 = Gamer_Reason[['whyplay', 'SWL_T']]
+    GamerSplitW3["Questionnaire Type"] = "SWL"
+    GamerSplitP3 = Gamer_Reason[['whyplay', 'SPIN_T']]
+    GamerSplitP3["Questionnaire Type"] = "SPIN"
 
-    Gamer_Reason_df.loc[len(Gamer_Reason_df)] = [Gamer_Reason_Total.drop(Gamer_Reason_df.index)['GAD_T'].sum(), Gamer_Reason_Total.drop(Gamer_Reason_df.index)['SWL_T'].sum(), Gamer_Reason_Total.drop(Gamer_Reason_df.index)['SPIN_T'].sum(), Gamer_Reason_Total.drop(Gamer_Reason_df.index)['Gamers In Category'].sum()]
-    Gamer_Reason_df = Gamer_Reason_df.rename(index={5: 'Others'})
+    GamerFlatten3 = pd.DataFrame({'whyplay': GamerSplitG3['whyplay'], 'Type': GamerSplitG3['Questionnaire Type'], 'Score': GamerSplitG3['GAD_T']})
+    GamerFlattenP3 = pd.DataFrame({'whyplay': GamerSplitP3['whyplay'], 'Type': GamerSplitP3['Questionnaire Type'], 'Score': GamerSplitP3['SPIN_T']})
+    GamerFlattenW3 = pd.DataFrame({'whyplay': GamerSplitW3['whyplay'], 'Type': GamerSplitW3['Questionnaire Type'], 'Score': GamerSplitW3['SWL_T']})
+    GamerFlatten3 = GamerFlatten3.append(GamerFlattenP3, ignore_index = True)
+    GamerFlatten3 = GamerFlatten3.append(GamerFlattenW3, ignore_index = True)
+    GamerFlatten3 = GamerFlatten3.sort_values('whyplay', ascending=False)
 
-    Gamer_Reason_df['GAD_Ave'] = Gamer_Reason_df['GAD_T'] / Gamer_Reason_df['Gamers In Category']
-    Gamer_Reason_df['SWL_Ave'] = Gamer_Reason_df['SWL_T'] / Gamer_Reason_df['Gamers In Category']
-    Gamer_Reason_df['SPIN_Ave'] = Gamer_Reason_df['SPIN_T'] / Gamer_Reason_df['Gamers In Category']
-    Gamer_Reason_df['Average Result'] = round(((Gamer_Reason_df['GAD_Ave']/GAD_TOTAL) + ((SWL_TOTAL - Gamer_Reason_df['SWL_Ave']) / SWL_TOTAL) + (Gamer_Reason_df['SPIN_Ave'] / SPIN_TOTAL)) / 3 , 4)
-
-    Playing_reasons = list(Gamer_Reason_df.index)
-    Average_result =  Gamer_Reason_df['Average Result']
-    reason_for_playing = plt.barh(Playing_reasons, Average_result)
-
-    df = px.data.tips()
-    fig = px.bar(reason_for_playing, Average_result, Playing_reasons, text=[f'{i:.2f}%' for i in Average_result*100], 
-                color = Playing_reasons, color_discrete_map={
-                    "having fun": "#FF7B89",
-                    "improving": "#8A5082",
-                    "winning": "#6F5F90",
-                    "relaxing": "#758EB7",
-                    "All of the above": "#A5CAD2",
-                    "Others": "#a0b0cf"}, 
-                    labels={
-                        "x": "Average Results (in percentage) for each reason for playing",
-                        "y": "Reasons for playing"
-                    }, title="How does the reason for playing affect the gamer’s anxiety levels?", orientation='h', width=800, height = 400)
-    fig.update_layout(showlegend=False)
-    fig.layout.xaxis.tickformat = '0.00%'
-    fig.update_layout(xaxis_range=[0,1])
-    graph4JSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-
-
+    Reason_fig = px.strip(GamerFlatten3, x='Score', y="Type", color='whyplay', color_discrete_map = {"having fun": "#FF7B89",
+                "improving": "#8A5082",
+                "winning": "#6F5F90",
+                "relaxing": "#758EB7",
+                "All of the above": "#A5CAD2",
+                "Others": "#a0b0cf"})
+    graph4JSON = json.dumps(Reason_fig, cls=plotly.utils.PlotlyJSONEncoder)
     return render_template('Reason.html', graph4JSON=graph4JSON)
 
 @app.route('/Final', methods = ['GET', 'POST'])
@@ -205,15 +185,18 @@ def Final():
         filter_gender = "Male"
         filter_reason = "having fun"
         filter_playstyle = "Singleplayer"
+
         
-        Filtering = GameAnxiety[['Age', 'Gender', 'whyplay', 'Playstyle',  'GAD_T', 'SWL_T', 'SPIN_T']]
+        Filtering = GameAnxiety[['Age', 'Gender', 'GAD_T', 'SWL_T', 'SPIN_T']]
+        Filtering['Playstyle'] = playstyle['Playstyle']
+        Filtering['whyplay'] = Gamer_Reason['whyplay'] 
         choices_gender = ['Male', 'Female', 'Others']
         choices_playstyle = ['Singleplayer', 'Multiplayer - Offline', 'Multiplayer - Online', 'All of the above', 'Others']
         choices_reasons = ['having fun', 'improving', 'winning', 'relaxing', 'All of the above', 'Others']
         n_age_group = 5
         Filtering['Age'] = pd.cut(Filtering['Age'], n_age_group)
-        age_groups = list(Gamer_Age_Total.index)
-        for i in age_groups:
+        
+        for i in intervals:
             if filter_age in i:
                 user_group = i
                 break
@@ -244,14 +227,16 @@ def Final():
         print(filter_reason)
         filter_playstyle = request.form['playstyle_input']
         print(filter_playstyle)
-        Filtering = GameAnxiety[['Age', 'Gender', 'whyplay', 'Playstyle',  'GAD_T', 'SWL_T', 'SPIN_T']]
+        Filtering = GameAnxiety[['Age', 'Gender', 'GAD_T', 'SWL_T', 'SPIN_T']]
+        Filtering['Playstyle'] = playstyle['Playstyle']
+        Filtering['whyplay'] = Gamer_Reason['whyplay'] 
         choices_gender = ['Male', 'Female', 'Others']
         choices_playstyle = ['Singleplayer', 'Multiplayer - Offline', 'Multiplayer - Online', 'All of the above', 'Others']
         choices_reasons = ['having fun', 'improving', 'winning', 'relaxing', 'All of the above', 'Others']
         n_age_group = 5
         Filtering['Age'] = pd.cut(Filtering['Age'], n_age_group)
-        age_groups = list(Gamer_Age_Total.index)
-        for i in age_groups:
+        
+        for i in intervals:
             if filter_age in i:
                 user_group = i
                 break
